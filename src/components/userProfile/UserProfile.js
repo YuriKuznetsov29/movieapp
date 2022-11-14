6d3a7ec80cde46f0177204e5ab99c29ffe69a2d0
Yuri <playeer2905@gmail.com>
2022-11-15 01:20:18 +0300

    addRemoveFilms

--- src/components/userProfile/UserProfile.js
+++ src/components/userProfile/UserProfile.js
@@ -1,26 +1,30 @@
 import { useState, useEffect } from 'react';
-import { getDatabase, ref, onValue} from "firebase/database";
+import { getDatabase, ref, onValue, update, remove, set} from "firebase/database";
 import { getAuth, onAuthStateChanged} from "firebase/auth";
 
 import './userProfile.scss'
 
 const UserProfile = (props) => {
     const [films, setFilms] = useState([]);
-
+    const [data, setData] = useState([]);
     useEffect(() => {
         autorizationStatus(auth)
     }, [])
 
     const auth = getAuth();
 
+    function writeNewPost(userId, key) {
+        
+      }
+
     const autorizationStatus = (auth) => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
               const uid = user.uid;
               const email = user.email;
-              console.log('send to db')
-              readData(uid)
-              console.log(films)
+              console.log('send to db');
+              readData(uid);
+              console.log(films);
             } else {
               console.log('User is signed out');
             }
@@ -31,9 +35,33 @@ const UserProfile = (props) => {
         const db = getDatabase();
         const Ref = ref(db, `users/` + userId + '/favoriteFilms/');
         onValue(Ref, (films) => {
-        const data = films.val()
-        setFilms(Object.values(data))
+        const data = films.val();
+        setData(data);
+        setFilms(Object.entries(data));
     })}
+
+    const deleteFavoriteFilm = (key) => {
+        
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const uid = user.uid;
+                const db = getDatabase();
+                let newData = Object.assign({}, data);
+                delete newData[key];
+                console.log(data)
+                console.log(newData)
+                const updates = {};
+                updates[`users/` + uid + `/favoriteFilms/`] = newData;
+                update(ref(db), updates)
+                // setFilms([]);
+                // console.log(films)
+                // readData(uid);
+                // renderFilms(films)
+            } else {
+              console.log('User is signed out');
+            }
+          });
+    }
 
     const renderFilms = (arr) => {
         const items = arr.map(item => {
@@ -42,17 +70,18 @@ const UserProfile = (props) => {
                     <div 
                         className='find-poster'
                         onClick={() => {
-                            props.onFilmSelected(item.id);
+                            props.onFilmSelected(item[1].id);
                         }}>
-                            <img src={item.poster} alt="logo"/>
+                            <img src={item[1].poster} alt="logo"/>
                     </div>
                     <div className='find-info'>
-                        <h4>{item.name}</h4>
-                        <div>{item.year}</div>
-                        <div>{item.genre}</div>
+                        <h4>{item[1].name}</h4>
+                        <div>{item[1].year}</div>
+                        <div>{item[1].genre}</div>
                         
                     </div>
-                    <i class="ph-trash delete"></i>
+                    <i class="ph-trash delete"
+                        onClick={() => deleteFavoriteFilm(item[0])}></i>
                 </div>
             )
         })
